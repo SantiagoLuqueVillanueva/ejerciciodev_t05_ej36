4a046e7a0ca4c9bd790ab6be542ba0c1647fbfbe
Santiago Luque Villanueva <luquevillanuevasantiago@gmail.com>
2025-11-05 18:46:00 +0100

    Done

--- src/App.java
+++ src/App.java
@@ -1,5 +1,18 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        long numeroIntroducido = Long.parseLong(System.console().readLine("Por favor, introduzca un número entero positivo: "));
+
+        long numero = numeroIntroducido;
+        long volteado = 0;
+
+        while (numero > 0) {
+            volteado = (volteado * 10) + (numero % 10);
+            numero /= 10;
+        }
+
+        if(volteado == numeroIntroducido)
+            System.out.printf("El %d es capicúa.", numeroIntroducido);
+        else
+            System.out.printf("El %d no es capicúa.", numeroIntroducido);
     }
 }
